cdfaeec26c03375b5e8b8f6cc30365c1dde37d31
AchuDexQBit <achu@dexqbit.com>
2026-03-23 21:07:59 +0000

    bench commands updated

--- railway/railway-setup.sh
+++ railway/railway-setup.sh
@@ -3,21 +3,22 @@ set -e
 
 echo "-> Create empty common site config"
 echo "{}" > /home/frappe/bench/sites/common_site_config.json
+chown frappe:frappe /home/frappe/bench/sites/common_site_config.json
 
 echo "-> Create new site with ERPNext"
-cd /home/frappe/bench
-bench new-site ${RFP_DOMAIN_NAME} \
+su frappe -c "cd /home/frappe/bench && bench new-site ${RFP_DOMAIN_NAME} \
     --admin-password ${RFP_SITE_ADMIN_PASSWORD} \
     --no-mariadb-socket \
     --db-root-password ${RFP_DB_ROOT_PASSWORD} \
-    --install-app erpnext
+    --install-app erpnext"
 
 echo "-> Install custom app"
-bench --site ${RFP_DOMAIN_NAME} install-app erpnext_wl_dqb_demo
+su frappe -c "cd /home/frappe/bench && bench --site ${RFP_DOMAIN_NAME} install-app erpnext_wl_dqb_demo"
 
-bench use ${RFP_DOMAIN_NAME}
+echo "-> Set default site"
+su frappe -c "cd /home/frappe/bench && bench use ${RFP_DOMAIN_NAME}"
 
 echo "-> Enable scheduler"
-bench enable-scheduler
+su frappe -c "cd /home/frappe/bench && bench enable-scheduler"
 
 echo "-> Setup complete!"
